8f6fd5e9d8907e51ee5f1c0cd64102f458e4adce
Narkay <sanvoisintinael@yahoo.fr>
2018-01-31 18:07:15 +0100

    CasesChanges
    AddCases, need to fill them

---
+++ Cases.py
@@ -0,0 +1,60 @@
+
+class Case:
+
+    def __init__(self, adjacent):
+        self.name="jojo"
+        self.adjacent = adjacent
+
+
+case1 = Case([1,2,3])
+print(case1.adjacent)
+
+"""
+case1 = Case
+        case2 = Case
+        case3 = Case
+        case4 = []
+        case5 = []
+        case6 = []
+        case7 = []
+        case8 = []
+        case9 = []
+        case10 = []
+        case11 = []
+        case12 = []
+        case13 = []
+        case14 = []
+        case15 = []
+        case16 = []
+        case17 = []
+        case18 = []
+        case19 = []
+        case20 = []
+        case21 = []
+        case22 = []
+        case23 = []
+        case24 = []
+        case25 = []
+        case26 = []
+        case27 = []
+        case28 = []
+        case29 = []
+        case30 = []
+        case31 = []
+        case32 = []
+        case33 = []
+        case34 = []
+        case35 = []
+        case36 = []
+        case37 = []
+        case38 = []
+        case39 = []
+        case40 = []
+        case41 = []
+        case42 = []
+        case43 = []
+        case44 = []
+"""
+
+
+
--- Unite.py
+++ Unite.py
@@ -1,3 +1,5 @@
+from Cases import *
+
 class Unite:
     def __init__(self):
         self.number = 0
@@ -13,6 +15,7 @@ class Unite:
 
 
         :return:
+
         """
 
         return
@@ -38,11 +41,28 @@ class Unite:
         """
         return
 
-    def Moves(self):
+
+    def Moves(self, listeCaseOccupee, typeCase, caseArrivee) :
+        "On prend les coordonnées de la case sur laquelle on clique pour savoir si on peut se déplacer sur cette case."
+        "Inutile de savoir toutes les cases possible seule la case sur laquelle le joueur veut se déplacer est importante"
+        "Il faut vérifier si la case d'arrivée est adjactentes à au moins case ocupée par le joueur"
         """Je conquéris
-        si rien sur plateau:
-            à côté de mer
-        sinon
-            à côté d'une case occupée
-        :return:"""
+                si rien sur plateau:
+                    à côté de mer
+                sinon
+                    à côté d'une case occupée
+                :return:"""
+
+        #if cliqueGauche == True
+        try :
+            if typeCase == "mer" :
+                raise TypeError("C'est une case mer")
+            else :
+                for element in listeCaseOccupee:
+                    if caseArrivee in listeCaseOccupee :
+                        return False
+                    if caseArrivee in Case.getAdjacent(element) :
+                        return True
+        except TypeError :
+            print("Ce n'est pas une case mer")
 
